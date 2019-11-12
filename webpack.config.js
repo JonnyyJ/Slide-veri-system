6460d91b8f20de80c2a1e64aebc2cccd835a01fe
Jonnyy <caijishiwo@gmail.com>
2019-11-11 22:45:33 -0500

    config

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,36 @@
-let path = require('path');
-let webpack = require('webpack');
+const path = require('path');
+
 module.exports = {
     entry: './index.js',
     output: {
-        path: path.join(__dirname, 'dist'), 
+        path: path.resolve(__dirname, 'dist'), 
         filename: "veri.js"
     },
     module: {
         rules: [
-
+            { // model for sass
+                test: /\.scss$/,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    "sass-loader"
+                ]
+            },
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.js$/,
+                use: ["source-map-loader"],
+                enforce: "pre"
+            }
         ]
-    }
+    },
 };
