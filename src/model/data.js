6e6e77eaeac465c7ebf11df96172121e0cf84bb4
Jonnyy <caijishiwo@gmail.com>
2019-11-20 17:00:23 -0500

    creating data model

---
+++ src/model/data.js
@@ -0,0 +1,28 @@
+// Those are the data that would store in font end
+
+// Initialize Link
+exports.initialize = "";
+
+// Link for large picture
+exports.largePicture = "";
+
+// Link for small picture
+exports.smallPicture = "";
+
+// Link for verification
+exports.verify = "";
+
+// Link for element's id that the zcapt would be created
+exports.id = "";
+
+// Verification ID
+exports.authID = "";
+
+// Small picture initial position in X
+exports.smallPictureInitialPositionX = 0;
+
+// Small picture initial position in Y
+exports.smallPictureInitialPositionY = 0;
+
+// verified?
+exports.isVerified = false;
